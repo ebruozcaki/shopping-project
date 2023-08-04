f526f418ba26aa6fbc28fc0c01138d01b68afbfb
Ebru Ozcaki <ebruozcaki14@gmail.com>
2023-08-04 11:28:27 +0300

    Updates on user input

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+"use strict";
 
+let input = document.querySelector(".category-input");
+let title = document.querySelector(".title");
+
+input.addEventListener("keydown", function (e) {
+  if (e.key === "Enter") {
+    title.prepend(input.value);
+    input.classList.add("hidden");
+  }
+});
