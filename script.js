2825afa45d821f10d2e60166a63a2afe58b55a4e
Ebru Ozcaki <ebruozcaki14@gmail.com>
2023-08-04 16:54:54 +0300

    Adding product trial

--- script.js
+++ script.js
@@ -2,10 +2,19 @@
 
 let input = document.querySelector(".category-input");
 let title = document.querySelector(".title");
+let addProduct = document.querySelector(".add-product");
+let deleteProduct = document.querySelector(".delete-product");
+let product = document.querySelector(".product");
 
 input.addEventListener("keydown", function (e) {
   if (e.key === "Enter") {
     title.prepend(input.value);
     input.classList.add("hidden");
   }
+});
+
+addProduct.addEventListener("click", function () {
+  const newProduct = document.createElement("section");
+  newProduct.classList.add("product");
+  document.querySelector(".category").appendChild(newProduct);
 });
