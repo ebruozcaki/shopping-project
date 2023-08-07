d6759b107339c288753a5cf5c502c1c6ad5d42f4
Ebru Ozcaki <ebruozcaki14@gmail.com>
2023-08-07 11:49:09 +0300

    Deleting product with 1 fault

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ let input = document.querySelector(".category-input");
 let title = document.querySelector(".title");
 let addProduct = document.querySelector(".add-product");
 let deleteProduct = document.querySelector(".delete-product");
-let product = document.querySelector(".product");
 
 input.addEventListener("keydown", function (e) {
   if (e.key === "Enter") {
@@ -13,6 +12,7 @@ input.addEventListener("keydown", function (e) {
   }
 });
 
+//Adding new product to a certain category
 addProduct.addEventListener("click", function () {
   const productSection = document.querySelector(".product");
   const newProduct = document.createElement("div");
@@ -21,6 +21,7 @@ addProduct.addEventListener("click", function () {
   const checkbox = document.createElement("input");
   checkbox.setAttribute("type", "checkbox");
   checkbox.classList.add("checkbox");
+  checkbox.checked = false;
   newProduct.appendChild(checkbox);
 
   const productInput = document.createElement("input");
@@ -44,4 +45,47 @@ addProduct.addEventListener("click", function () {
   productSection.appendChild(newProduct);
 
   checkbox.classList.add("hidden");
+});
+
+//Deleting product(s) from a certain category
+//NEED UPDATE TO CHECKBOX OF PREVIOUS PRODUCTS
+deleteProduct.addEventListener("click", function () {
+  let checkboxes = document.querySelectorAll(".checkbox");
+  for (let i = 0; i < checkboxes.length; i++) {
+    let checkboxToBeChecked = checkboxes[i];
+    checkboxes[i].classList.remove("hidden");
+    //Checkboxes check&uncheck
+    checkboxToBeChecked.addEventListener("change", function () {
+      console.log("here");
+      if (checkboxToBeChecked.checked === true) {
+        check(checkboxToBeChecked);
+        console.log("checked");
+      } else {
+        uncheck(checkboxToBeChecked);
+        console.log("unchecked");
+      }
+    });
+  }
+  //Deleting products of which checkboxes are checked
+  deleteProduct.addEventListener("click", function () {
+    const products = document.querySelectorAll(".product");
+    for (let i = 0; i < products.length; i++) {
+      let productToBeDeleted = products[i];
+      if (productToBeDeleted.childNodes[0].checked === true) {
+        productToBeDeleted.remove();
+      }
+    }
+    for (let i = 0; i < checkboxes.length; i++) {
+      checkboxes[i].classList.add("hidden");
+    }
+  });
+
+  //Checking and unchecking functions
+  function check(checkbox) {
+    checkbox.checked === true;
+  }
+
+  function uncheck(checkbox) {
+    checkbox.checked === false;
+  }
 });
