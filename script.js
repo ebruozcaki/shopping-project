e20d08f53b1bf8ce4b903e3be0a69b0c21560523
Ebru Ozcaki <ebruozcaki14@gmail.com>
2023-08-11 17:40:47 +0300

    Local Storage: cleared

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 "use strict";
-
+localStorage.clear();
 let budgetInput = document.getElementById("budget-input");
 let budgetRemained = document.getElementById("budget-remained");
 let newCategoryBtn = document.querySelector(".new-category");
